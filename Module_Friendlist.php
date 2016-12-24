d78c32b863a65fec6abe802630d2c2f251520045
gizmore <gizmore@wechall.net>
2016-12-24 23:00:40 +0100

    Friendships moved to core.

--- Module_Friendlist.php
+++ Module_Friendlist.php
@@ -4,10 +4,10 @@ final class Module_Friendlist extends GWF_Module
 	##############
 	### Module ###
 	##############
-	public function getVersion() { return 4.00; }
+	public function getVersion() { return 4.01; }
 	public function getDefaultPriority() { return 20; }
 	public function getDefaultAutoLoad() { return true; }
-	public function getClasses() { return array('GWF_Friendship', 'GWF_FriendRequest'); }
+	public function getClasses() { return array('GWF_FriendRequest'); }
 	public function onLoadLanguage() { return $this->loadLanguage('lang/friendlist'); }
 	public function onInstall($dropTable) { require_once 'GWF_InstallFriendlist.php'; return GWF_InstallFriendlist::onInstall($this, $dropTable); }
 	public function onCronjob() { require_once 'GWF_FriendlistCronjob.php'; GWF_FriendlistCronjob::onCronjob($this); }
